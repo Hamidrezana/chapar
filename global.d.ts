08566ac52b5a41d9e6c803ca770da9699d63cad6
Hamidreza Nazemi <hamidrezanazemi16@gmail.com>
2022-08-28 11:54:30 +0430

    fix: fix callback types

--- global.d.ts
+++ global.d.ts
@@ -7,6 +7,6 @@ declare global {
   type $DeepPartial<T> = { [P in keyof T]?: $DeepPartial<T[P]> };
   type $NullType<T> = T | null;
   type $NullUndefinedType<T> = T | null | undefined;
-  type $VoidFunc = () => void;
+  type $VoidFunc = () => void | undefined;
   type $MapType<T> = Record<number | string, T>;
 }
